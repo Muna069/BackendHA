bf3238a4eed41df12d45fe07a4581f6748601b13
Muna069 <118343472+Muna069@users.noreply.github.com>
2025-04-24 22:25:16 +0300

    Update exerciseRoutes.js

--- healthApp-backend-main/routes/exerciseRoutes.js
+++ healthApp-backend-main/routes/exerciseRoutes.js
@@ -372,7 +372,7 @@ router.post("/assign-exercise", async (req, res) => {
 });
 
 // AI Exercise Assignment Cron Job (Runs at 6:00 AM Daily)
-cron.schedule("55 8 * * *", async () => {
+cron.schedule("0 6 * * *", async () => {
   try {
     console.log("Running AI exercise assignment job...");
 
