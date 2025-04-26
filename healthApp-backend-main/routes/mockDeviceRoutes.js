e22f49808ecd159d4b8fb9cf18e05a552d847f17
Muna069 <118343472+Muna069@users.noreply.github.com>
2025-04-26 10:51:05 +0300

    Update mockDeviceRoutes.js

--- healthApp-backend-main/routes/mockDeviceRoutes.js
+++ healthApp-backend-main/routes/mockDeviceRoutes.js
@@ -141,7 +141,7 @@ router.get("/history/:userId", async (req, res) => {
 
 router.get("/history/:userId/:date", async(req, res)=>{
   try{
-    const date = new Date(req.params.date);
+    const date = req.params.date;
     const history = await DailyDeviceData.find({userId: req.params.userId, date: date});
     res.json(history);
   }catch(err){
