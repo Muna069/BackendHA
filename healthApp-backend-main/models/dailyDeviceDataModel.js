cf985ce455b0729c9332853c7eaf7defc59d6093
Muna069 <118343472+Muna069@users.noreply.github.com>
2025-04-27 19:23:16 +0300

    Update dailyDeviceDataModel.js

--- healthApp-backend-main/models/dailyDeviceDataModel.js
+++ healthApp-backend-main/models/dailyDeviceDataModel.js
@@ -1,12 +1,12 @@
 const mongoose = require("mongoose");
 
-const DailyDataSchema = new mongoose.Schema({
-    userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-    date: { type: Date, required: true }, 
-    sleepMinutes: { type: Number, required: true },
-    heartRate: { type: Number, required: true },
-    stepsCount: { type: Number, required: true },
-    caloriesBurned: { type: Number, required: true },
+const DailyDeviceDataSchema = new mongoose.Schema({
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  date: { type: Date, required: true },
+  sleepMinutes: { type: Number, required: true },
+  heartRate: { type: Number, required: true },
+  stepsCount: { type: Number, required: true },
+  caloriesBurned: { type: Number, required: true },
 });
 
-module.exports = mongoose.model("DailyData", DailyDataSchema);
+module.exports = mongoose.model("DailyDeviceData", DailyDeviceDataSchema);
