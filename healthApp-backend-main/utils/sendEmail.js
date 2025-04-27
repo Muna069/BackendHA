b7a5b0d81a43e3d983055e9e5c364ae916d124b1
Muna069 <118343472+Muna069@users.noreply.github.com>
2025-04-28 02:06:02 +0300

    Update sendEmail.js

--- healthApp-backend-main/utils/sendEmail.js
+++ healthApp-backend-main/utils/sendEmail.js
@@ -3,19 +3,24 @@ const nodemailer = require('nodemailer');
 
 async function sendEmail(to, subject, text) {
   const transporter = nodemailer.createTransport({
-    service: 'Gmail',
+    service: 'Gmail', // You can replace this with another service if needed
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
     },
   });
 
-  await transporter.sendMail({
-    from: process.env.EMAIL_USER,
-    to,
-    subject,
-    text,
-  });
+  try {
+    await transporter.sendMail({
+      from: process.env.EMAIL_USER,
+      to,
+      subject,
+      text,
+    });
+    console.log('Email sent successfully');
+  } catch (error) {
+    console.error('Error sending email:', error);
+  }
 }
 
 module.exports = sendEmail;
