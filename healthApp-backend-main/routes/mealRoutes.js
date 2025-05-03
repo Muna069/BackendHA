5579dfac56cb3f9dbbd7ca0e7b15fe07cd43f806
Muna069 <118343472+Muna069@users.noreply.github.com>
2025-05-03 16:49:01 +0300

    Update mealRoutes.js

--- healthApp-backend-main/routes/mealRoutes.js
+++ healthApp-backend-main/routes/mealRoutes.js
@@ -200,7 +200,7 @@ router.get("/ai-meal-recommendation/:userId", async (req, res) => {
 });
 
 // Cron job to generate daily AI meal & hydration recommendations for all users
-cron.schedule("30 6 * * *", async () => {
+cron.schedule("58 16 * * *", async () => {
   try {
       console.log("Running AI meal & hydration recommendation job...");
 
